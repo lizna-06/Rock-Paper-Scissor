402a20934b6f90225a338ec0d15ab6dc09b4a571
lizna-06 <liznamolla@gmail.com>
2025-08-30 21:37:14 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,48 @@
+import random
+import sys
+c1=0
+c2=0
+def game():
+    global c1,c2 #tells the program to use the given global variables
+    if userchoice==compchoice:
+        c1+=1
+        c2+=1
+        print("It's a tie !")
+    elif (userchoice=="rock" and compchoice=="paper")or(userchoice=="paper" and compchoice=="scissor")or(userchoice=="scissor" and compchoice=="rock"):
+        c2+=1
+        print("Uh-Oh, You lose!")
+        
+    else:
+        c1+=1
+        print("Yay,you win!")
+       
+
+  
+options=["rock","paper","scissor"]
+print("WELCOME TO ROCK PAPER SCISSOR GAME!")
+print("type 'exit' to exit the game") 
+while True:
+    userchoice=input("enter rock/paper/scissor: ").lower()
+    if userchoice=="exit":
+        print("THANKS FOR PLAYING!")
+        print("Your Score= ", c1)
+        print("Computer's Score= ",c2)
+        if(c2>c1):
+            print("Winner= Computer")
+        elif(c1>c2):
+            print("Winner= You")
+        else:
+            print("Its a tie between you and the computer")
+        sys.exit() #exits the program
+    if userchoice not in options:
+        print("invalid choice, please try again")
+        continue # starts over the loop and asks again
+           
+    compchoice=random.choice(options)
+    print(f"computer chooses: {compchoice}")
+
+    game()
+
+       
+
+    
